6412b6b9054d7a7f7afb5083e996d2bb861d2232
Rich Williamson <richard.williamson@thecurve.io>
2023-07-14 21:39:19 +0100

    User seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts 'Seeding Users'
+User.create(
+  email: "test@domain.com",
+  password: "password123",
+  username: "test-user",
+  full_name: "Test User",
+  phone_number: "07789123456"
+)
+
+puts 'Seeding Users - Done'
