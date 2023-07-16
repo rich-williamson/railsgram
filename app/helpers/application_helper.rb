55d0c2ab24c0ce0c98fdc59809daef56388fd191
Rich Williamson <richard.williamson@thecurve.io>
2023-07-16 21:27:31 +0100

    Better post layout with carousels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def formatted_date_with_suffix(date)
+    day = date.day
+    suffix = case day % 10
+             when 1 then 'st'
+             when 2 then 'nd'
+             when 3 then 'rd'
+             else 'th'
+             end
+
+    date.strftime("#{day}#{suffix} %B %Y")
+  end
 end
